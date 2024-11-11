23e9c1198cb0ac4596a09c4f7dbc7fa166405f5d
kar2n <31457499+kar2n@users.noreply.github.com>
2024-11-11 15:19:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,100 @@
 import streamlit as st
+
+if "role" not in st.session_state:
+  st.session_state.role = None
+
+ROLES = [None, "Requester", "Responder", "Admin"]
+
+def login():
+  st.header("log in")
+  role = st.selectbox("Choose you role", ROLES)
+  
+  if st.button("Log in"):
+    st.session_state.role = role
+    st.rerun()
+
+
+def logout():
+  st.session_state = None
+  st.rerun()
+
+
+role = st.session_state.role
+
+logout_page = st.Page(logout, title = "log out", icon=":material/logout:")
+
+settings = st.Page("settings.py", title="settings", icon=":material/settings")
+
+request_1 = st.Page(
+  "request/request_1.py",
+  title="request 1",
+  icon=":material/help:",
+  default=(role == "Requester")
+)
+request_2 = st.Page(
+   "request/request_2.py", title="request 2", icon=":material/bug_report:",
+)
+
+respond_1 = st.Page(
+  "respond/respond_1.py",
+  title="respond 1",
+  icon=":material/healing:",
+  default=(role == "Responder")
+)
+respond_2 = st.Page(
+  "respond/respond_2.py", title="respond 2",   icon=":material/handyman:"
+)
+
+admin_1 = st.Page(
+  "admin/admin_1.py",
+  title="admin 1",
+  icon=":material/person_add:",
+  default=(role == "Admin")
+)
+admin_2 = st.Page(
+  "admin/admin_2.py", title="admin 2", icon=":material/security:"
+)
+
+
+account_pages = [logout_page, settings]
+request_pages = [request_1, request_2]
+respond_pages = [responde_1, respond_2]
+admin_pages = [admin_1, admin_2]
+
+
+# defining common elements and navigation
+
+st.title("Request manager")
+st.logo("images/horizontal.png", icon_image="images/icon.png")
+
+page_dict = {}
+
+if st.session_state.role in ["Requester", "Admin"]:
+  page_dict["Request"] = request_pages
+if st.session_state.role in ["Responder", "Admin"]:
+  page_dict["Respond"] = respond_pages
+if st.session_state.role == "Admin":
+  page_dict["Admin"] = admin_pages  
+
+if len(page_dict) > 0:
+  pg = st.navigation{"Account": account_pages} | page_dict)
+else:
+  pg = st.navigation([st.Page(login)])
+
+pg.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
